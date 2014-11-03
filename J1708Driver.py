4608f021972ffb1d15908f48b6c0a72056dbc410
James Johnson <james@synercontechnologies.com>
2014-11-03 17:06:49 +0000

    initial addition of J1708Driver.py

---
+++ J1708Driver.py
@@ -0,0 +1,53 @@
+import socket
+import sys
+import struct
+from functools import reduce
+
+def toSignedChar(num):
+        if type(num) is bytes:
+                return struct.unpack('b',num)[0]
+        else:
+                return struct.unpack('b',struct.pack('B',num & 0xFF))[0]
+
+def checksum(msg):
+#        msg = list(map(ord,msg))
+        return toSignedChar(~reduce(lambda x,y: (x + y) & 0xFF, list(msg)) + 1)
+
+
+class J1708Driver():
+    #serveport: the port the the J1708 Driver. Defaults to the ECM driver, which is on port 6969
+    #clientport: the port to listen on. The ECM driver sends to port 6970. Will add a method to register clients for more flexibility.
+    def __init__(self,serveport=6969,clientport=6970):
+        self.serveport = serveport
+        self.clientport = clientport
+        self.sock = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
+        self.sock.bind(('localhost',self.clientport))
+
+    #checksum: Checksum included in return value if True. Defaults to false.
+    #returns the message read as bytes type.
+    def read_message(self,checksum=False):
+        message = self.sock.recv(1024)
+        if checksum:
+            return message
+        else:
+            return message[:-1]
+
+    #buf: message to send as type bytes
+    #has_check: True if your message includes checksum. Defaults to False.
+    def send_message(self,buf,has_check=False):
+        msg = buf
+        if not has_check:
+            check = struct.pack('b',checksum(msg))
+            msg += check
+        self.sock.sendto(msg,('localhost',self.serveport))
+
+#Test to see if this works. Reads 10 messages, sends a CAT ATA SecuritySetup message.
+#You should see a reply of the form \x80\xfe\xac\xf0\x?? if it works
+if __name__ == '__main__':
+    driver = J1708Driver()
+    for i in range(0,10):
+        print(repr(driver.read_message()))
+    
+    driver.send_message(b'\xAC\xFE\x80\xF0\x17')
+    for i in range(0,10):
+        print(repr(driver.read_message()))
